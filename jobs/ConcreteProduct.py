363e701eded278477c646e324627f7f1e9da1b3b
Jowzera <51763929+Jowzera@users.noreply.github.com>
2024-05-06 16:14:19 +0100

    Factory method placed

---
+++ jobs/ConcreteProduct.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+
+class Product(ABC):
+    """
+    The Product interface declares the operations that all concrete products
+    must implement.
+    """
+
+    @abstractmethod
+    def operation(self) -> str:
+        pass
+
+
+"""
+Concrete Products provide various implementations of the Product interface.
+"""
+
+
+class ConcreteProduct1(Product):
+    def operation(self) -> str:
+        return "{Result of the ConcreteProduct1}"
+
+
+class ConcreteProduct2(Product):
+    def operation(self) -> str:
+        return "{Result of the ConcreteProduct2}"
